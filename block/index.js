1534d0a23801f5c6d31f163ada6980955022a17e
Brian Hogg <bhogg@carnegietechnologies.com>
2018-12-29 23:11:42 -0500

    Adding description for the block in the settings sidebar

--- block/index.js
+++ block/index.js
@@ -7,7 +7,8 @@ const { applyFilters } = wp.hooks;
 const {	registerBlockType } = wp.blocks;
 
 registerBlockType( 'events-calendar-shortcode/block', {
-	title: __( 'The Events Calendar Block', 'events-calendar-shortcode' ),
+	title: __( 'The Events Calendar Block', 'the-events-calendar-shortcode' ),
+	description: __( 'Display your events from The Events Calendar', 'the-events-calendar-shortcode' ),
 	icon: logo,
 	category: 'common',
 	supports: {
